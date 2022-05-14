1d001f3759c58da5f2de1a983163363e549feea5
ADSM <yesform2018@gmail.com>
2022-05-14 23:11:19 +0600

    first commit

---
+++ src/Components/Expriences/Expriences.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Expriences.css'
+
+const Expriences = () => {
+    return (
+        <div>
+            <h1>Expriences</h1>
+        </div>
+    );
+};
+
+export default Expriences;
