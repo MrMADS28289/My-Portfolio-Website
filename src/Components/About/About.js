49f50e29e760ebc7a0daf608108be3925365679e
ADSM <yesform2018@gmail.com>
2022-05-15 03:26:07 +0600

    About Me  Section is completed

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,10 +1,50 @@
 import React from 'react';
 import './About.css'
+import me from '../../assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa'
 
 const About = () => {
     return (
         <section id='about'>
-            <h1>About</h1>
+
+            <h5>Get To Know</h5>
+            <h2>About Me</h2>
+
+            <div className="container about_container">
+                <div className="about_me">
+                    <img src={me} alt="" />
+                </div>
+                <div className="container about_content">
+                    <div className="about_cards">
+
+                        <article className="about_card">
+                            <FaAward className='about_icon' />
+                            <h5>Exprience</h5>
+                            <small>3+ years</small>
+                        </article>
+
+                        <article className="about_card">
+                            <FaAward className='about_icon' />
+                            <h5>Exprience</h5>
+                            <small>3+ years</small>
+                        </article>
+
+                        <article className="about_card">
+                            <FaAward className='about_icon' />
+                            <h5>Exprience</h5>
+                            <small>3+ years</small>
+                        </article>
+
+                    </div>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, amet natus, error architecto modi aperiam, vitae ad inventore vero consectetur minus. Praesentium quod ea id numquam! Ipsum explicabo sed provident.
+                    </p>
+
+                    <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+
+                </div>
+            </div>
         </section>
     );
 };
--- src/Components/Expriences/Expriences.js
+++ src/Components/Expriences/Expriences.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './Expriences.css'
+import { BsPatchCheckFill } from 'react-icons/bs'
 
 const Expriences = () => {
     return (
         <section id='expriences'>
-            <h1>expriences</h1>
+            <h5>What Skills I Have</h5>
+            <h2>My Exprience</h2>
+
+            <div className="container exprience_container">
+                <div className="exprience_frontend">
+                    <h3>Frontend Development</h3>
+                    <div className="exprience_contant">
+                        <article className='exprience_details'>
+                            <BsPatchCheckFill />
+                        </article>
+                    </div>
+                </div>
+                <div className="exprience_backend">
+
+                </div>
+            </div>
         </section>
     );
 };
