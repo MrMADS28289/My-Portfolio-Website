95d821feedd9c3654da9f3af7e59989e685b4601
ADSM <yesform2018@gmail.com>
2022-05-15 01:23:39 +0600

    Header Section is completed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import CTA from './CTA';
+import me from '../../assets/me.png'
+import HeaderSocial from './HeaderSocial';
 
 const Header = () => {
     return (
-        <div>
-            <h1>header</h1>
-        </div>
+        <header>
+            <div className="container header_container">
+                <h5>Hello i'm</h5>
+                <h1>Mr. Mads</h1>
+                <h5 className="text-light">Web Developer</h5>
+                <CTA />
+                <HeaderSocial />
+                <div className="me">
+                    <img src={me} alt="" />
+                </div>
+                <a href="#contact" className='scroll_down'>Scrool Down</a>
+            </div>
+        </header>
     );
 };
 
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import './NavBar.css'
+import './NavBar.css';
+import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai';
+import { BiBook, BiMessageSquareDetail } from 'react-icons/bi';
+import { RiServiceLine } from 'react-icons/ri';
 
 const NavBar = () => {
     return (
-        <div>
-            <h1>nav</h1>
-        </div>
+        <nav>
+            <a href="#"><AiOutlineHome /></a>
+            <a href="#about"><AiOutlineUser /></a>
+            <a href="#expriences"><BiBook /></a>
+            <a href="#services"><RiServiceLine /></a>
+            <a href="#"><BiMessageSquareDetail /></a>
+        </nav>
     );
 };
 
