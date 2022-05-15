3e3d716cf93f23be8b9ae8f259c1f2f1475c56c6
ADSM <yesform2018@gmail.com>
2022-05-16 05:36:21 +0600

    Contact  Setction is completed

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Contact.css'
+import { MdOutlineMail } from 'react-icons/md';
+import { RiMessengerLine } from 'react-icons/ri';
+import { BsWhatsapp } from 'react-icons/bs';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_w1kk0y5', 'template_5ub3fgd', form.current, 'vw3RhUyXqF_NMoCFu')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
+    };
+
     return (
         <section id='contact'>
-            <h1>Contact</h1>
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+
+            <div className="container contact_container">
+                <div className="contact_options">
+                    <article className="contact_option">
+                        <MdOutlineMail className='contact_option-icon' />
+                        <h4>Email</h4>
+                        <h5>yesform2018@gmail.com</h5>
+                        <a href="mailto:yesform2018@gmail.com" target='_blank'>Send a message</a>
+                    </article>
+
+                    <article className="contact_option">
+                        <RiMessengerLine className='contact_option-icon' />
+                        <h4>Messenger</h4>
+                        <h5>Mr. Mads</h5>
+                        <a href="https://m.me/Mr.Mads2" target='_blank'>Send a message</a>
+                    </article>
+
+                    <article className="contact_option">
+                        <BsWhatsapp className='contact_option-icon' />
+                        <h4>Whatsapp</h4>
+                        <h5>+8801734326573</h5>
+                        <a href="https://api.whatsapp.com/send?phone+8801734326573" target='_blank'>Send a message</a>
+                    </article>
+                </div>
+                {/* END OF CONTACT OPTION */}
+
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type="text" name='name' placeholder='Your Full Name' required />
+                    <input type="email" name="email" placeholder='Your Email' required id="" />
+                    <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+                    <button type="submit" className='btn btn-primary'>Send Message</button>
+                </form>
+
+            </div>
         </section>
     );
 };
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './Footer.css';
+import { MdFacebook } from 'react-icons/md';
+import { AiFillTwitterCircle, AiFillInstagram } from 'react-icons/ai';
+import { IoLogoLinkedin } from 'react-icons/io';
 
 const Footer = () => {
     return (
-        <div>
-            footer
-        </div>
+        <footer>
+            <ul className='social_icon'>
+                <li><a href=""><MdFacebook className='footer_icon' /></a></li>
+                <li><a href=""><AiFillTwitterCircle className='footer_icon' /></a></li>
+                <li><a href=""><IoLogoLinkedin className='footer_icon' /></a></li>
+                <li><a href=""><AiFillInstagram className='footer_icon' /></a></li>
+            </ul>
+            <ul className="menu">
+                <li><a href="#home"><p className='menu_lin'>Home</p></a></li>
+                <li><a href="#about"><p className='menu_lin'>About Me</p></a></li>
+                <li><a href="#expriences"><p className='menu_lin'>Exprience</p></a></li>
+                <li><a href="#services"><p className='menu_lin'>Service</p></a></li>
+                <li><a href="#contact"><p className='menu_lin'>Contact</p></a></li>
+            </ul>
+        </footer>
     );
 };
 
